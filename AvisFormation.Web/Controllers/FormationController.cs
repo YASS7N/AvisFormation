cd9e81c8352ff3bf570471c8a0db4ee536685aae
dell <dell@YASS7N>
2025-01-02 23:20:56 +0100

    Add project files.

---
+++ AvisFormation.Web/Controllers/FormationController.cs
@@ -0,0 +1,37 @@
+﻿using AvisFormation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvisFormation.Web.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult ToutesLesFormations()
+        {
+            var list = new List<Formation>();
+            using (var context = new AvisFormationDBEntities())
+            {
+                list = context.Formation.ToList();
+            }
+                return View(list);
+        }
+        public ActionResult DetailsFormation(string nomseo)
+        {
+            var formation = new Formation();
+            using (var context = new AvisFormationDBEntities())
+            {
+                formation = context.Formation.Where(f => f.NomSeo.Equals(nomseo)).FirstOrDefault();
+            
+            if(formation == null)
+            {
+                return RedirectToAction("ToutesLesFormations");
+            } 
+            return View(formation);
+            }
+        }
+    }
+}
